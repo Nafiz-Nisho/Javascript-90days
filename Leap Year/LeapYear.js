ad7bb70037411e345cfcaa64bd466c4f21b37472
nafiz <nafizahmedcrypto@gmail.com>
2022-10-15 03:25:59 +0600

    LeapYearAdd

---
+++ Leap Year/LeapYear.js
@@ -0,0 +1,34 @@
+
+
+// Leap year holo onk imporportent ekti function javascript a. 
+
+
+function  LeapYearCalculation (years)
+
+{ 
+    if(years %4 ==0) {
+
+    return true
+    
+}
+
+     else {
+
+        console.log ("eta Leap year na")
+
+        return false 
+    }
+
+}
+   //  Leap Year ar framwork banay nilam . akhon hbe moja ! 
+
+
+
+   const amarrLeapYearList = (2103)
+
+   const AmarTA = LeapYearCalculation (amarrLeapYearList)
+  
+   console.log (AmarTA)
+
+
+//    OUTPUT = eta Leap year na
