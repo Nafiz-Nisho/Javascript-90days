4a9af232e0f6996cd076ccbe896657d277945e29
nafiz <nafizahmedcrypto@gmail.com>
2022-10-23 11:48:23 +0600

    fibonacci

--- factorial/factorialRecursive.js
+++ factorial/factorialRecursive.js
@@ -1,4 +1,12 @@
-function factorialRec  (n) {
+
+        //      সূত্র  
+
+        //  Simple way : 1x2x3x4x5x6 
+        //   ---       : 6 = (6-1) x 8
+
+        // Javascript way : -n = n x (x-1)
+
+function factorialRecursive (n) {
 
     // Eta ekta condition
     if (n == 0) {
@@ -9,16 +17,16 @@ function factorialRec  (n) {
 
     // Condition number 2
     else {
-     return n * factorialRec (n-1)
+     return n * factorialRecursive (n-1)
     }
     // Conditon number END 
     
 }
 
-var result = factorialRec (4)
+var result = factorialRecursive (5)
 
 console.log (result)
 
-
+// OUTPUT 120
 
 // Aro valo kore buzte chaile : https://www.youtube.com/watch?v=wySKkHmwwIo
